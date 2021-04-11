d0348b5843e63e176390bbf32d74fecf6c897f8b
Alex <cigan@ua.fm>
2021-04-11 17:07:46 +0300

    feat: add a solution of the fourth task: to create a Minesweeper game setup

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -1,11 +1,11 @@
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
  * that don't contain a mine have a number in it that indicates the total number of mines
- * in the neighboring cells. Starting off with some arrangement of mines
+ * in the neighboring cells. Starting off with some newArrangement of mines
  * we want to create a Minesweeper game setup.
  *
- * @param {Array<Array>} matrix
- * @return {Array<Array>}
+ * @param {newArray<newArray>} matrix
+ * @return {newArray<newArray>}
  *
  * @example
  * matrix = [
@@ -21,8 +21,51 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+
+function minesweeper(matrix) {
+  const newArr = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    newArr.push([]);
+    for (let y = 0; y < matrix[i].length; y++) {
+      let counter = 0;
+      const fieldX = i - 1;
+      const fieldY = y - 1;
+      const fieldX1 = i + 1;
+      const fieldY1 = y + 1;
+
+      if (matrix[fieldX] && matrix[fieldX][fieldY]) {
+        counter++;
+      }
+      if (matrix[fieldX] && matrix[fieldX][y]) {
+        counter++;
+      }
+      if (matrix[fieldX] && matrix[fieldX][fieldY1]) {
+        counter++;
+      }
+      if (matrix[i][fieldY]) {
+        counter++;
+      }
+      if (matrix[i][fieldY1]) {
+        counter++;
+      }
+      if (matrix[fieldX1] && matrix[fieldX1][fieldY]) {
+        counter++;
+      }
+      if (matrix[fieldX1] && matrix[fieldX1][y]) {
+        counter++;
+      }
+      if (matrix[fieldX1] && matrix[fieldX1][fieldY1]) {
+        counter++;
+      }
+      newArr[i].push(counter);
+    }
+  }
+  return newArr;
 }
+
+// const matrix = [[true, false, false], [false, true, false], [false, false, false]];
+// test = minesweeper(matrix);
+// console.log(test);
 
 module.exports = minesweeper;
